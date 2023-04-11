c8519df703d1bce0a715a032619e5c35964ccc3c
gurpal-04 <96622200+gurpal-04@users.noreply.github.com>
2023-04-11 19:07:01 +0530

    formatted

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import pandas as pd
 
+df = pd.read_excel('/home/runner/GFGHack/assets/toughestsport.xlsx')
+
 app = Flask(__name__)
 
 
@@ -9,40 +11,32 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+
 @app.route('/getvalue', methods=['POST'])
-# def data():
-#   df = pd.read_excel('/home/runner/GFGHack/assets/toughestsport.xlsx')
-#   return df
-# def check(l,sport,df):
-#   sport = sport[0].upper()+sport[1:].lower()
-#   s=(df.loc[df['SPORT'] == s])
-#   p=['END',	'STR',	'PWR',	'SPD',	'AGI',	'FLX',	'NER',	'DUR',	'HAN',	'ANA']
-#   s = s[p].values
-#   o=[]
-#   for i in range(10):
-#     if l[i]< s[0][i]:
-#       o.append(p[i])
-#   return o
 def getvalue():
-    # df = data()
-    df = pd.read_excel('/home/runner/GFGHack/assets/toughestsport.xlsx')
-    feed = [request.form['feedback_text'],request.form['feedback_text1'],request.form['feedback_text2'],request.form['feedback_text3'],request.form['feedback_text4'],request.form['feedback_text5'],request.form['feedback_text6'],request.form['feedback_text7'],request.form['feedback_text8'],request.form['feedback_text9']]
-    print(feed)
-    
-    sport = request.form['sport']
-    # o=check(feed,sport,df)
-    sport = sport[0].upper()+sport[1:].lower()
-    s=(df.loc[df['SPORT'] == sport])
-    p=['END',	'STR',	'PWR',	'SPD',	'AGI',	'FLX',	'NER',	'DUR',	'HAN',	'ANA']
-    s = s[p].values
-    o=[]
-    for i in range(10):
-      if int(feed[i])< int(s[0][i]):
-        o.append(p[i])
-    
-    
-    
-    return render_template("output.html", f=feed,s=o)
+
+  feed = [
+    request.form['parameter1'], request.form['parameter2'],
+    request.form['parameter3'], request.form['parameter4'],
+    request.form['parameter5'], request.form['parameter6'],
+    request.form['parameter7'], request.form['parameter8'],
+    request.form['parameter9'], request.form['parameter10']
+  ]
+  # print(feed)
+
+  sport = request.form['sport']
+
+  sport = sport[0].upper() + sport[1:].lower()
+  s = (df.loc[df['SPORT'] == sport])
+  p = ['END', 'STR', 'PWR', 'SPD', 'AGI', 'FLX', 'NER', 'DUR', 'HAN', 'ANA']
+  s = s[p].values
+  o = []
+  for i in range(10):
+    if int(feed[i]) < int(s[0][i]):
+      o.append(p[i])
+
+  return render_template("output.html", f=feed, s=o)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=81, debug=True)
