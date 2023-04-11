f4d0c09ec9e359e9ecaef201a1818b57c31cd27e
gurpal-04 <96622200+gurpal-04@users.noreply.github.com>
2023-04-11 18:52:18 +0530

    First commit
    functions only for scroll labels

---
+++ templates/index.js
@@ -0,0 +1,40 @@
+
+function updateEndurance(val){
+  document.getElementById('END_input').innerHTML = val;
+}
+
+function updateStrength(val){
+    document.getElementById('STR_input').innerHTML = val;
+}
+
+function updatePower(val){
+    document.getElementById('POW_input').innerHTML = val;
+}
+
+function updateSpeed(val){
+    document.getElementById('SPD_input').innerHTML = val;
+}
+
+function updateAgility(val){
+    document.getElementById('AGI_input').innerHTML = val;
+}
+
+function updateFlexibility(val){
+    document.getElementById('FLX_input').innerHTML = val;
+}
+
+function updateNerve(val){
+    document.getElementById('NVR_input').innerHTML = val;
+}
+
+function updateDurability(val){
+    document.getElementById('DUR_input').innerHTML = val;
+}
+
+function updateHAN(val){
+    document.getElementById('HAN_input').innerHTML = val;
+}
+
+function updateANA(val){
+    document.getElementById('ANA_input').innerHTML = val;
+}
